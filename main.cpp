a50ce55f95874a3737127c84ae53a64306aa4a9d
aragami3070 <smirnov17612@gmail.com>
2024-10-16 10:30:33 +0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+using namespace std;
+
+int main(){
+    // матрица расстояний между местоположениями, где первый double --- растояние между двумя пунктами, второй --- вероятность пойти в этот пункт
+    vector<vector<pair<double, double>>> table;
+}
